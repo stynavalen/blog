546f8f1427b899db621118bea169b25d37252d78
vs <stynavalen@gmail.com>
2019-06-19 14:59:13 +0300

    Changed the HTML for the site.

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,5 +1,14 @@
-"""mysite URL Configuration
+"""mysite URL Configuration"""
+from django.contrib import admin
+from django.urls import path, include
 
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('blog.urls')),
+]
+
+
+"""comments from dj
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/2.0/topics/http/urls/
 Examples:
@@ -13,9 +22,3 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-]
